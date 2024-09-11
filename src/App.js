98d9f20c6c41d9dea6ca9df44eafe1737da26d6a
Yoshitaro Takaesu <useakat@gmail.com>
2024-09-11 13:24:56 +0000

    LLL added to OKK

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. OKK!
+          Edit <code>src/App.js</code> and save to reload. OKKLLLL!
         </p>
         <a
           className="App-link"
